a45d7abc4169e4ce049e502682469402b1bb62b1
Atílio <atiliodadalto@protonmail.com>
2024-03-10 12:16:31 -0300

    feat: create Config.lua

---
+++ Short Rest Cooldown/Mods/ShortRestCooldown/ScriptExtender/Lua/Shared/Helpers/Config.lua
@@ -0,0 +1,25 @@
+Config = Helpers.Config:New({
+  folderName = "ShortRestCooldown",
+  configFilePath = "short_rest_cooldown_config.json",
+  defaultConfig = {
+    GENERAL = {
+      enabled = true, -- Toggle the mod on/off
+    },
+    FEATURES = {
+      cooldown = 1, -- Cooldown in seconds
+      -- only_in_multiplayer = false, -- If true, the cooldown will only be applied in multiplayer
+    },
+    DEBUG = {
+      level = 0 -- 0 = no debug, 1 = minimal, 2 = verbose logs
+    }
+  },
+  onConfigReloaded = {}
+})
+
+Config:UpdateCurrentConfig()
+
+Config:AddConfigReloadedCallback(function(configInstance)
+  SRCPrinter.DebugLevel = configInstance:GetCurrentDebugLevel()
+  SRCPrint(0, "Config reloaded: " .. Ext.Json.Stringify(configInstance:getCfg(), { Beautify = true }))
+end)
+Config:RegisterReloadConfigCommand("src")
