5cc1ed8f57b8532834c8b8caa72679f007ca2f7f
Atílio <atiliodadalto@protonmail.com>
2024-03-10 10:56:59 -0300

    feat: add Printer.lua for logging utility functions

---
+++ Short Rest Cooldown/Mods/ShortRestCooldown/ScriptExtender/Lua/Shared/Helpers/Printer.lua
@@ -0,0 +1,30 @@
+SRCPrinter = VolitionCabinetPrinter:New { Prefix = "SRC", ApplyColor = true, DebugLevel = Config:GetCurrentDebugLevel() }
+
+function SRCPrint(debugLevel, ...)
+  SRCPrinter:SetFontColor(0, 255, 255)
+  SRCPrinter:Print(debugLevel, ...)
+end
+
+function SRCTest(debugLevel, ...)
+  SRCPrinter:SetFontColor(100, 200, 150)
+  SRCPrinter:PrintTest(debugLevel, ...)
+end
+
+function SRCDebug(debugLevel, ...)
+  SRCPrinter:SetFontColor(200, 200, 0)
+  SRCPrinter:PrintDebug(debugLevel, ...)
+end
+
+function SRCWarn(debugLevel, ...)
+  SRCPrinter:SetFontColor(255, 100, 50)
+  SRCPrinter:PrintWarning(debugLevel, ...)
+end
+
+function SRCDump(debugLevel, ...)
+  SRCPrinter:SetFontColor(190, 150, 225)
+  SRCPrinter:Dump(debugLevel, ...)
+end
+
+function SRCDumpArray(debugLevel, ...)
+  SRCPrinter:DumpArray(debugLevel, ...)
+end
